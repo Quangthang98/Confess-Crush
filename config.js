ccee5cbaa025a8f59e54ea4841b02b9fb8ce7826
Quangthang98 <124145745+Quangthang98@users.noreply.github.com>
2023-02-01 22:44:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Sẳn sàng trả lời câu hỏi chưa",
+    introTitle: 'Đoàn Lê Quang thắng',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
